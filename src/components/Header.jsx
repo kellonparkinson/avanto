701188c405714396d7bc785ca5a25d49660eca18
Kellon Parkinson <kellon.digital@gmail.com>
2023-01-04 16:00:25 -0700

    initial upload for Avanto

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BiMenu } from 'react-icons/bi'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h2>Avanto</h2>
+        <BiMenu size='35px' />
+    </div>
+  )
+}
+
+export default Header
