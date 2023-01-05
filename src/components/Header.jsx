f9f861ada486811337f722859e3d19230a4c19f8
Kellon Parkinson <kellon.digital@gmail.com>
2023-01-05 16:19:26 -0700

    created menu and added navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiMenu } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleMenu = () => !setShowMenu(!showMenu)
+
   return (
-    <div className='header'>
+    <header>
         <h2>Avanto</h2>
-        <BiMenu size='35px' />
-    </div>
+        <BiMenu size='35px' style={{zIndex: 3}} onClick={handleMenu} />
+        <nav className={showMenu ? 'menu-show' : 'menu-hide'}>
+          <Link to='/'>
+            <button onClick={handleMenu}>Home</button>
+          </Link>
+          <Link to='/auth'>
+            <button onClick={handleMenu}>Login</button>
+          </Link>
+        </nav>
+    </header>
   )
 }
 
---
+++ src/screens/AdminScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminScreen = () => {
+  return (
+    <div className='main-page'>
+      <h1>Admin</h1>
+    </div>
+  )
+}
+
+export default AdminScreen
